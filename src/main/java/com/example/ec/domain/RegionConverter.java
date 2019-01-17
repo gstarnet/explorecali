745f0e28cb48f21fb64cfb6c353d472064238e7e
Gregory Drogaline <greg.drogaline@gmail.com>
2019-01-16 21:24:54 -0600

    1.4 Identify transactional business services boundaries - start

--- src/main/java/com/example/ec/domain/RegionConverter.java
+++ src/main/java/com/example/ec/domain/RegionConverter.java
@@ -3,6 +3,11 @@ package com.example.ec.domain;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+/**
+ * Hibernate Converter for the Region Enum to DB Column.
+ *
+ * Created by Mary Ellen Bowman
+ */
 @Converter(autoApply = true)
 public class RegionConverter implements AttributeConverter<Region, String> {
     @Override
